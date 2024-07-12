9982eb8721ee661d3e4d41d92e4445a6703fa097
Killian <Killian.sheriff@gmail.com>
2024-07-12 14:44:47 +0000

    parsing

--- devel.py
+++ devel.py
@@ -108,7 +108,7 @@ class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
             )
             subprocess.run(command, check=True, shell=True)
 
-        # TODO: Analyse lammps outputs
+        # Analyse lammps outputs
         data = np.loadtxt("output/lammps_preFL.log", unpack=True)
         _, equilibrium_lattice_parameters, spring_constants = data[:,0],data[:,1],data[:,2]
 
