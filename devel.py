01a6a4a33d1908bb86872669b12fb7268e2cccfd
Killian <Killian.sheriff@gmail.com>
2024-07-12 20:39:29 +0000

    updates

--- devel.py
+++ devel.py
@@ -11,8 +11,8 @@ from kim_tools import CrystalGenomeTestDriver
 class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
     def _calculate(
         self,
-        temperatures: List[float],
-        pressures: List[float],
+        temperature: float,
+        pressure: float,
         size: Tuple[int, int, int],
         **kwargs,
     ) -> None:
@@ -20,49 +20,49 @@ class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
         # TODO: Docstring
         """
         # Check arguments
-        if not np.all(np.array(temperatures) > 0.0):
+        if not temperature > 0.0:
             raise ValueError("Temperature has to be larger than zero.")
 
-        if not np.all(np.array(pressures) > 0.0):
+        if not pressure > 0.0:
             raise ValueError("Pressure has to be larger than zero.")
 
         # Write initial atomic structure to lammps dump file
         supercell = self._write_initial_structure(size)
 
-        # preFL computes the lattice parameter and spring constants as functions of pressure at the starting temperature.
-        # TODO: Does it work for anythign else than cubic structures? 
+        # preFL computes the equilibrium lattice parameter and spring constants for a given temperature and pressure.
+        # TODO: This should probably be replaced with its own test driver, which compute equilibrium lattice constants, and which can handles arbitrary crystal structures. Then we can get spring constants.
         equilibrium_lattice_parameters, spring_constants = self._preFL(
-            pressures=pressures, temperature=np.min(temperatures)
+            pressurs=pressure, temperature=temperature
         )
 
-        # TODO: Need to scale self.atoms by the equilibrium lattice constant, write it to a dump file and load it in FL
+        # Rescaling 0K supercell to have equilibrium lattice constant.
         breakpoint()
-
-        # FL computes the free energy as a function of pressure at the starting temperature (list of free energies vs P at T = starting temperature).
-        free_energies_vs_pressure_at_temperature = self._FL(
-            pressures=pressures, temperature=np.min(temperatures)
+        supercell.write(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "output/equuilibrium_crystal.dump",
+            ),
+            format="lammps-data",
+            masses=True,
         )
 
-        # RS computes the free energy at each pressure over a mesh of temperatures.
-        free_energies_vs_pressure_vs_temperature = self._RS(
-            pressures=pressures, temperature=np.min(temperatures)
-        )
+        # FL computes the free energy at a given pressure and temperature.
+        free_energy = self._FL(pressures=pressure, temperature=temperature)
 
         # Print Result
         print("####################################")
         print("# Frenkel Ladd Free Energy Results #")
         print("####################################")
 
-        print(
-            r"$G_{FL} =$" + f" {free_energies_vs_pressure_vs_temperature:.5f} (eV/atom)"
-        )
+        print(r"$G_{FL} =$" + f" {free_energy:.5f} (eV/atom)")
 
         # I have to do this or KIM tries to save some coordinate file
         self.poscar = None
 
         # Write property
-        # TODO: Write them using kim utils helps for writting kim properties.
-        self._add_key_to_current_property_instance("constant_pressure_free_energy", XX, "eV/atom")
+        self._add_key_to_current_property_instance(
+            "free_energy", free_energy, "eV/atom"
+        )
 
     def _write_initial_structure(
         self,
@@ -87,39 +87,35 @@ class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
             os.path.dirname(os.path.realpath(__file__)), filename
         )
         atoms_new.write(structure_file, format="lammps-data", masses=True)
-       
 
         return atoms_new
 
     def _preFL(
-        self, pressures: List[float], temperature: float
+        self, pressure: float, temperature: float
     ) -> Tuple[List[float], List[float]]:
 
-        for pressure in pressures:
-        
-
-            variables = {
-                "modelname": self.kim_model_name,
-                "temperature": temperature,
-                "temperature_seed": seed,
-                "temperature_damping": tdamp,
-                "pressure": pressure,
-                "pressure_damping": pdamp,
-                "timestep": timestep,
-                "number_sampling_timesteps": number_sampling_timesteps,
-                "species": " ".join(self.species),
-                "average_position_filename": "output/average_position_equilibration.dump.*",
-                "average_cell_filename": "output/average_cell_equilibration.dump",
-                "write_restart_filename": "output/final_configuration_equilibration.restart",
-            }
-            # TODO: Possibly run MPI version of Lammps if available.
-            command = (
-                "lammps "
-                + " ".join(f"-var {key} '{item}'" for key, item in variables.items())
-                + " -log output/lammps_preFL.log"
-                + " -in lammps_templates/preFL_template.lmp"
-            )
-            subprocess.run(command, check=True, shell=True)
+        variables = {
+            "modelname": self.kim_model_name,
+            "temperature": temperature,
+            "temperature_seed": seed,
+            "temperature_damping": tdamp,
+            "pressure": pressure,
+            "pressure_damping": pdamp,
+            "timestep": timestep,
+            "number_sampling_timesteps": number_sampling_timesteps,
+            "species": " ".join(self.species),
+            "average_position_filename": "output/average_position_equilibration.dump.*",
+            "average_cell_filename": "output/average_cell_equilibration.dump",
+            "write_restart_filename": "output/final_configuration_equilibration.restart",
+        }
+        # TODO: Possibly run MPI version of Lammps if available.
+        command = (
+            "lammps "
+            + " ".join(f"-var {key} '{item}'" for key, item in variables.items())
+            + " -log output/lammps_preFL.log"
+            + " -in lammps_templates/preFL_template.lmp"
+        )
+        subprocess.run(command, check=True, shell=True)
 
         # Analyse lammps outputs
         data = np.loadtxt("output/lammps_preFL.log", unpack=True)
@@ -131,69 +127,34 @@ class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
 
         return equilibrium_lattice_parameters, spring_constants
 
-    def _FL(self, pressures: List[float], temperature: float) -> List[float]:
+    def _FL(self, pressure: float, temperature: float):
 
-        for pressure in pressures:
-
-            variables = {
-                "modelname": self.kim_model_name,
-                "temperature": temperature,
-                "temperature_seed": seed,
-                "temperature_damping": tdamp,
-                "pressure": pressure,
-                "pressure_damping": pdamp,
-                "timestep": timestep,
-                "number_sampling_timesteps": number_sampling_timesteps,
-                "species": " ".join(self.species),
-                "average_position_filename": "output/average_position_equilibration.dump.*",
-                "average_cell_filename": "output/average_cell_equilibration.dump",
-                "write_restart_filename": "output/final_configuration_equilibration.restart",
-            }
-            # TODO: Possibly run MPI version of Lammps if available.
-            command = (
-                "lammps "
-                + " ".join(f"-var {key} '{item}'" for key, item in variables.items())
-                + " -log output/lammps_FL.log"
-                + " -in lammps_templates/FL_template.lmp"
-            )
-            subprocess.run(command, check=True, shell=True)
+        variables = {
+            "modelname": self.kim_model_name,
+            "temperature": temperature,
+            "temperature_seed": seed,
+            "temperature_damping": tdamp,
+            "pressure": pressure,
+            "pressure_damping": pdamp,
+            "timestep": timestep,
+            "number_sampling_timesteps": number_sampling_timesteps,
+            "species": " ".join(self.species),
+            "average_position_filename": "output/average_position_equilibration.dump.*",
+            "average_cell_filename": "output/average_cell_equilibration.dump",
+            "write_restart_filename": "output/final_configuration_equilibration.restart",
+        }
+        # TODO: Possibly run MPI version of Lammps if available.
+        command = (
+            "lammps "
+            + " ".join(f"-var {key} '{item}'" for key, item in variables.items())
+            + " -log output/lammps_FL.log"
+            + " -in lammps_templates/FL_template.lmp"
+        )
+        subprocess.run(command, check=True, shell=True)
 
         # TODO: Analyse lammps outputs
         free_energies_vs_pressure_at_temperature = []
         return free_energies_vs_pressure_at_temperature
-
-    def _RS(
-        self,
-        pressures: List[float],
-    ):
-        for pressure in pressures:
-
-            variables = {
-                "modelname": self.kim_model_name,
-                "temperature": temperature,
-                "temperature_seed": seed,
-                "temperature_damping": tdamp,
-                "pressure": pressure,
-                "pressure_damping": pdamp,
-                "timestep": timestep,
-                "number_sampling_timesteps": number_sampling_timesteps,
-                "species": " ".join(species),
-                "average_position_filename": "output/average_position_equilibration.dump.*",
-                "average_cell_filename": "output/average_cell_equilibration.dump",
-                "write_restart_filename": "output/final_configuration_equilibration.restart",
-            }
-            # TODO: Possibly run MPI version of Lammps if available.
-            command = (
-                "lammps "
-                + " ".join(f"-var {key} '{item}'" for key, item in variables.items())
-                + " -log output/lammps_RS.log"
-                + " -in lammps_templates/RS_template.lmp"
-            )
-            subprocess.run(command, check=True, shell=True)
-
-        # TODO: Analyse lammps outputs
-        free_energies_vs_pressure_vs_temperature = []
-        return free_energies_vs_pressure_vs_temperature
 
 
 if __name__ == "__main__":
@@ -203,6 +164,6 @@ if __name__ == "__main__":
     test_driver(
         bulk("Ar", "fcc", a=5.248),
         size=(3, 3, 3),
-        temperatures=[10.0, 20.0, 30.0],
-        pressures=[1.0, 2.0, 3.0],
+        temperature=100.0,
+        pressure=1.0,
     )
