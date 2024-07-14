260e317d08602bd805145f14bdadbecdc31d1a28
Jason Ogbebor <81113182+JOgbebor@users.noreply.github.com>
2024-07-14 14:14:06 -0400

    Update devel.py
    Tdamp changed

--- devel.py
+++ devel.py
@@ -134,7 +134,7 @@ class FrenkelLaddFreeEnergies(CrystalGenomeTestDriver):
         variables = {
             "modelname": self.kim_model_name,
             "temperature": self.temperature,
-            "temperature_damping": 0.1,
+            "temperature_damping": 1.0,
             "temperature_seed": np.random.randint(low=100000, high=999999, dtype=int),
             "pressure": self.pressure,
             "pressure_damping": 1.0,
