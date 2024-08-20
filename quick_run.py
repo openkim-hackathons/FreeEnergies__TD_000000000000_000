b70a982416294715ada1f589145be39790032d6c
Jason Ogbebor <jogbebor@mit.edu>
2024-08-20 01:07:41 +0000

    Added some silicon test results

--- quick_run.py
+++ quick_run.py
@@ -3,19 +3,19 @@ from test_driver.test_driver import TestDriver
 import subprocess
 from kim_tools import query_crystal_genome_structures
 
-kim_model_name ="EAM_Dynamo_ErcolessiAdams_1994_Al__MO_123629422045_005"
+#kim_model_name ="EAM_Dynamo_ErcolessiAdams_1994_Al__MO_123629422045_005"
 #kim_model_name = "SW_StillingerWeber_1985_Si__MO_405512056662_006" # Stillinger-Weber
 #kim_model_name = "EDIP_JustoBazantKaxiras_1998_Si__MO_958932894036_002" # EDIP
-#kim_model_name = "SNAP_ZuoChenLi_2019_Si__MO_869330304805_000" # SNAP
+kim_model_name = "SNAP_ZuoChenLi_2019_Si__MO_869330304805_000" # SNAP
 #kim_model_name = "SNAP_ZuoChenLi_2019quadratic_Si__MO_721469752060_000" # qSNAP
 subprocess.run(f"kimitems install {kim_model_name}", shell=True, check=True)
 test_driver = TestDriver(kim_model_name)
 
 list_of_queried_structures = query_crystal_genome_structures(
     kim_model_name=kim_model_name,
-    stoichiometric_species=['Al'],
-    prototype_label="A_cF4_225_a",
-    #prototype_label="A_cF8_227_a", # cubic diamond
+    stoichiometric_species=['Si'],
+    #prototype_label="A_cF4_225_a",
+    prototype_label="A_cF8_227_a", # cubic diamond
     #prototype_label="A_hP4_194_f", # hexagonal diamond
 )
 
