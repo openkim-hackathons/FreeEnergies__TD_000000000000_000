05e47f2c523cd3726726366b7e3b85daa1b63e75
Killian <Killian.sheriff@gmail.com>
2025-05-19 17:55:43 +0000

    format

--- test_driver/test_driver.py
+++ test_driver/test_driver.py
@@ -59,6 +59,7 @@ class TestDriver(SingleCrystalTestDriver):
         if not np.allclose(cauchy_stress[:3], cauchy_stress[0], rtol=1e-5, atol=1e-8):
             raise ValueError("The first three components of the Cauchy stress tensor must be equal (calculation are run at constant isotropic stress).")
         self.pressure = -cauchy_stress[0] 
+        
         self.atoms = self._get_atoms()
         
         self._validate_inputs()
