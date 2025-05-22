6fa93453e47594a156ac4eff04981d3d92f2c5a2
Killian <Killian.sheriff@gmail.com>
2025-05-22 21:02:00 +0000

    add plotting

---
+++ examples/01_Silicon/02_plot.py
@@ -0,0 +1,12 @@
+from cte import FREE_ENERGIES, TEMPERATURES
+
+# Read in the computed free-energies from the edn files
+
+
+
+
+
+
+
+
+# Plot the computed free-energies against the reference free-energies
