419d95ef0a94d92281c6ecd2c85a9bb82de2f53d
Killian <Killian.sheriff@gmail.com>
2025-05-22 21:02:32 +0000

    imports

--- examples/01_Silicon/02_plot.py
+++ examples/01_Silicon/02_plot.py
@@ -1,4 +1,13 @@
+import lovelyplots
+import matplotlib.pyplot as plt
 from cte import FREE_ENERGIES, TEMPERATURES
+
+plt.style.use('paper')
+
+
+
+
+
 
 # Read in the computed free-energies from the edn files
 
